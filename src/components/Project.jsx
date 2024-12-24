c353b1c142a7a704008d323e1cc2488f191c166c
DeKUTInnovatorsClub <dekutinnovators23@gmail.com>
2024-12-24 13:20:51 +0300

    create projects page

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,8 +5,8 @@ export default function Project(){
             <img src={umemePhoto} alt="project photo" />
             <div className="project-details">
                 <h2 style={{fontSize:"1.5rem"}}>Umeme Sense</h2>
-                <p>an advanced real-time and remote smart energy meter that uses LoRa technology and data analytics to provide insights into energy consumption. It helps industries optimize usage, reduce costs, and enhance sustainability with its user-friendly interface and predictive analytics features. </p>
-                <p>Want to collaborate or more details? Call: +254700000002</p>
+                <p>An advanced real-time and remote smart energy meter that uses LoRa technology and data analytics to provide insights into energy consumption. It helps industries optimize usage, reduce costs, and enhance sustainability with its user-friendly interface and predictive analytics features. </p>
+                <p>Want to collaborate or need more details? Call: +254700000002</p>
             </div>    
         </div>
     )
